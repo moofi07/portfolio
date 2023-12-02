39709cac1d8f891df7104d62a4ae290ec5b1532a
moofi07 <moofikabegam@gmail.com>
2023-12-02 14:57:09 +0530

    portfolio code

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',function(){
+var typed = new Typed(" .text",{
+    Strings:["Frontend Developer" , "Java Developer" , "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay : 1000,
+    loop:true
+
+});
+
+});
